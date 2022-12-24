9ef46fe719288c8b381e78b16e9189eaf9f7fee5
HarshitaDwivedi72 <51255281+HarshitaDwivedi72@users.noreply.github.com>
2022-12-24 14:43:16 +0530

    android

---
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,115 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @react-native-async-storage/async-storage
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
+// @react-native-community/art
+import com.reactnativecommunity.art.ARTPackage;
+// @react-native-community/geolocation
+import com.reactnativecommunity.geolocation.GeolocationPackage;
+// @react-native-community/netinfo
+import com.reactnativecommunity.netinfo.NetInfoPackage;
+// @react-native-community/progress-bar-android
+import com.reactnativecommunity.androidprogressbar.RNCProgressBarPackage;
+// @react-native-picker/picker
+import com.reactnativecommunity.picker.RNCPickerPackage;
+// react-native-android-location-enabler
+import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
+// react-native-camera
+import org.reactnative.camera.RNCameraPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-get-location
+import com.github.douglasjunior.reactNativeGetLocation.ReactNativeGetLocationPackage;
+// react-native-localization
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+// react-native-maps
+import com.airbnb.android.react.maps.MapsPackage;
+// react-native-permissions
+import com.zoontek.rnpermissions.RNPermissionsPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-sqlite-storage
+import org.pgsqlite.SQLitePluginPackage;
+// react-native-svg
+import com.horcrux.svg.SvgPackage;
+// react-native-vector-icons
+import com.oblador.vectoricons.VectorIconsPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new AsyncStoragePackage(),
+      new ARTPackage(),
+      new GeolocationPackage(),
+      new NetInfoPackage(),
+      new RNCProgressBarPackage(),
+      new RNCPickerPackage(),
+      new RNAndroidLocationEnablerPackage(),
+      new RNCameraPackage(),
+      new RNGestureHandlerPackage(),
+      new ReactNativeGetLocationPackage(),
+      new ReactNativeLocalizationPackage(),
+      new MapsPackage(),
+      new RNPermissionsPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new SQLitePluginPackage(),
+      new SvgPackage(),
+      new VectorIconsPackage()
+    ));
+  }
+}
