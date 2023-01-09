fa4ee18d2f53cc80357362e9b8aed7868646526b
tanmay12-sud0 <shikharagarwal1414@gmail.com>
2023-01-09 20:37:55 +0530

    'ui

--- src/components/StartEndDetails.js
+++ src/components/StartEndDetails.js
@@ -10,7 +10,19 @@ export default function StartEndDetails() {
 
   
   const [data, setData] = useState();
-  const [printData, setPrintData] = useState([]);
+  const [printData, setPrintData] = useState([{
+    "_id": "63bc00ba4587976c951c2170",
+    "tripID": "Mon Jan 09 2023 17:25:14 GMT+0530UI001",
+    "userID": "UI001",
+    "date": "2023-01-09T11:55:37.682Z",
+    "startTime": "10:00AM",
+    "vehicleNumber": "Gfvh",
+    "startKilometer": "2555",
+    "startVehicleImageUrl": "https://storage.googleapis.com/logistiex-shp/DSQC/front/2023/0/9/SI002/rn_image_picker_lib_temp_dad8377e-4f00-47eb-af11-55c5842c35d6_1673265334736.jpg",
+    "createdAt": "2023-01-09T11:55:38.785Z",
+    "updatedAt": "2023-01-09T11:55:38.785Z",
+    "__v": 0
+    }]);
   const navigation = useNavigation();
 
     const getData = 'https://bked.logistiex.com/UserTripInfo/getUserTripInfo';
@@ -24,7 +36,7 @@ export default function StartEndDetails() {
                 Alert.alert(error);
             });
         })();
-    }, [printData]);
+    }, []);
 
 const getDataLocal = async () => {
   try {
@@ -41,123 +53,48 @@ const getDataLocal = async () => {
     console.log(e);
   }
 }
-
-console.log('dcdc')
+  console.log(printData, 'print')
   
     return (
-
       printData.length ?
       (
         <NativeBaseProvider>
         <ScrollView>
-        <Box flex={1} bg="coolGray.100" p={4}>
         
-    
         <Box
-          _light={{ bg: "coolGray.50" }}
-          _dark={{ bg: "coolGray.900" }}
-          justifyContent="center"
-          px={4}
-          
-        >
-          <VStack space={5} alignItems="center">
-            
-            <Heading size="lg">Detail</Heading>
-            <HStack alignItems="center" space={4} justifyContent="space-between">
-              <HStack alignItems='center'>
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      Start Time
-                  </Text>
-              </HStack>
-              <HStack style={{ width: 200 }} alignItems="center">
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      {printData[0].startTime}
-                  </Text>
-              </HStack>
-            </HStack>
+  shadow="2"
+  rounded="lg"
+  w='100%'
+  _light={{ bg: "coolGray.50" }}
+  _dark={{ bg: "gray.700" }}
+  marginTop={5}
+>
+  
+    <View style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}} >
+    <Image style={{height:200, width:350}} source={{uri : printData[0].startVehicleImageUrl}} alt="image base" />
+  <Text bold position="absolute" color="coolGray.50" top="0" m="4">
+    Start vehicle
+  </Text>
+  <Image marginTop={10} style={{height:200, width:350}} source={{uri : printData[0].startVehicleImageUrl}} alt="image base" />
+  <Text bold position="absolute" color="coolGray.50" top="40" m="20">
+  End vehicle
+  </Text>
+    </View>
+  <Stack space="2" p="4">
+    <Text style={{backgroundColor:'#004aad', paddingVertical: '3%',textAlign:'center', display:'flex', justifyContent:'center', alignItems:'center', color:'white'}}>Start Time - {printData[0].startTime}</Text>
+    <Text style={{backgroundColor:'#004aad', paddingVertical: '3%',textAlign:'center', display:'flex', justifyContent:'center', alignItems:'center', color:'white'}}>Vehicle Number - {printData[0].vehicleNumber}</Text>
+    <Text style={{backgroundColor:'#004aad', paddingVertical: '3%',textAlign:'center', display:'flex', justifyContent:'center', alignItems:'center', color:'white'}}>Start Kilometer - {printData[0].startKilometer}</Text>
+    <Text style={{backgroundColor:'#004aad', paddingVertical: '3%',textAlign:'center', display:'flex', justifyContent:'center', alignItems:'center', color:'white'}}>End Kilometer - {printData[0].startKilometer}</Text>
+  </Stack>
+  <HStack space="3" px="4" pb="4">
+   
+  </HStack>
+</Box>
 
-            <HStack alignItems="center" space={4} justifyContent="space-between">
-              <HStack alignItems='center'>
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      Start Kilometer
-                  </Text>
-              </HStack>
-              <HStack style={{ width: 200 }} alignItems="center">
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      {printData[0].startKilometer}
-                  </Text>
-              </HStack>
-            </HStack>
-
-            <HStack alignItems="center" space={4} justifyContent="space-between">
-              <HStack alignItems='center'>
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      End Kilometer
-                  </Text>
-              </HStack>
-              <HStack style={{ width: 200 }}  alignItems="center">
-                  <Text textAlign='center' alignItems='center' justifyContent='center' color="black" _dark={{
-                      color: "gray.400"
-                      
-                      }} fontWeight="400">
-                      {printData[0].startKilometer}
-                  </Text>
-              </HStack>
-            </HStack>
-
-            <HStack alignItems="center" space={4} justifyContent="space-between">
-              <HStack alignItems='center'>
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      Start Image
-                  </Text>
-              </HStack>
-              <HStack alignItems="center">
-              <Image 
-              source={{ uri: printData[0].startVehicleImageUrl}} 
-              style={{ width: 200, height: 200 }} 
-              alt = 'image not shown'
-                    />
-              </HStack>
-            </HStack>
-            
-
-            <HStack alignItems="center" space={4} justifyContent="space-between">
-              <HStack alignItems='center'>
-                  <Text color="black" _dark={{
-                      color: "gray.400"
-                      }} fontWeight="400">
-                      End Image
-                  </Text>
-              </HStack>
-              <HStack alignItems="center">
-              <Image 
-              source={{ uri: printData[0].startVehicleImageUrl}} 
-              style={{ width: 200, height: 200 }} 
-              alt = 'image not shown'
-                    />
-              </HStack>
-            </HStack>
-            
-
-
-          </VStack>
-        </Box>
           <Center>
             <Image style={{ width: 150, height: 100 }} source={require('../assets/image.png')} alt={"Logo Image"} />
           </Center>
-        </Box>
+        
         </ScrollView>              
       </NativeBaseProvider>
       ) : (
@@ -165,43 +102,7 @@ console.log('dcdc')
           <Text>Loading</Text>
         </NativeBaseProvider>
       )
-      
-      // <NativeBaseProvider>
-      //     <Box flex={1} bg="#004aad" alignItems="center" pt={'4%'} height={"100%"} >
-      //         <Box justifyContent="space-between" py={2} px={6} height={"80%"} bg="#fff" rounded="xl" width={"90%"} maxWidth="100%" _text={{fontWeight: "medium",}}>
-      //         <ScrollView>
-      //         {
-      //             printData &&
-      //             printData.map((res, i) =>{
-      //                 return(
-      //                   <ScrollView key={i}>
-      //                     <View>
-      //                     <Button title="Login" backgroundColor='#004aad'  _text={{ color: 'white', fontSize: 20 }}>{res.tripID}</Button>
-      //                     <Button title="Login" backgroundColor='#004aad'  _text={{ color: 'white', fontSize: 20 }}>Vehicle Number {res.vehicleNumber}</Button>
-      //                     <Button title="Login" backgroundColor='#004aad'  _text={{ color: 'white', fontSize: 20 }}>startTime {res.startTime}</Button>
-      //                     <Button title="Login" backgroundColor='#004aad'  _text={{ color: 'white', fontSize: 20 }}>startKilometer {res.startKilometer}</Button>
-      //                   <View style={styles.container}>
-      //                     <Image
-      //                       style={styles.stretch}
-      //                       source={{uri : res.startVehicleImageUrl}}
-      //                     />
-      //                   </View>
-      //                   </View>
-      //                   </ScrollView>
-      //                 )
-      //             })
-      //         }
-      //         </ScrollView>
-      //     </Box>
-      //     <Center>
-      //         <Image style={{ width: 200, height: 200 }} source={require('../assets/logo.png')} alt={"Logo Image"} />
-              
-      //     </Center>
-      //     </Box>
-      // </NativeBaseProvider>
     );
-
-  
 }
 
 
